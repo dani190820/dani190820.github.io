11ec447288433766814ff83b6e1e6c1a425c7583
dani190820 <153797322+dani190820@users.noreply.github.com>
2023-12-14 01:37:06 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensaje() {
+    document.getElementById("btnMostrar").style.display = "none";
+    document.getElementById("mensaje").style.display = "block";
+}
+
+function mostrarSiguiente() {
+    document.getElementById("mensaje").style.display = "none";
+    document.getElementById("mensajeFinal").style.display = "block";
+}
